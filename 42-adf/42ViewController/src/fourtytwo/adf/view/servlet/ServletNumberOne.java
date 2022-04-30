74533f4f2f25c950dc3985bef756d62be07d5ec3
martins.korulis <martins.korulis@gmail.com>
2022-04-30 07:43:38 +0300

    Some updates: servlet one returns js now; angualar app consumes the js served; snipped added

--- 42-adf/42ViewController/src/fourtytwo/adf/view/servlet/ServletNumberOne.java
+++ 42-adf/42ViewController/src/fourtytwo/adf/view/servlet/ServletNumberOne.java
@@ -2,6 +2,8 @@ package fourtytwo.adf.view.servlet;
 
 import java.io.IOException;
 
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +14,26 @@ public class ServletNumberOne extends HttpServlet {
     public ServletNumberOne() {
         super();
     }
-    
+
     @Override
-    protected void doGet(HttpServletRequest httpServletRequest,
-                         HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        super.doGet(httpServletRequest, httpServletResponse);
+    public void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/javascript");
+        PrintWriter out = response.getWriter();
+        writeJS(out);
+        out.close();
+    }
+    
+    private void writeJS(PrintWriter out){
+        out.println("var observer = new MutationObserver(function(mutations) {");
+        out.println("   if (document.contains(document.body)) {");
+        out.println("        var myElem = document.createElement('div');");
+        out.println("        myElem.innerHTML = 'Hello world';");
+        out.println("        myElem.classList.add('superbclass');");
+        out.println("        document.body.appendChild(myElem);");
+        out.println("        observer.disconnect();");
+        out.println("    }");
+        out.println("});");
+        out.println("observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});");
     }
 }
---
+++ 42-snippets/first.js
@@ -0,0 +1,11 @@
+var observer = new MutationObserver(function(mutations) {
+   if (document.contains(document.body)) {
+        var myElem = document.createElement('div');
+        myElem.innerHTML = 'Hello world';
+        myElem.childList.add('superbclass');
+        document.body.appendChild(myElem);
+        observer.disconnect();
+    }
+});
+
+observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
