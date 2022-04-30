a71688121f555403c8e1109835392531b272d502
Viktors Certkovs <ext.viktors.sa.certkovs@valtion.onmicrosoft.com>
2022-04-30 17:04:20 +0300

    hope this is last

--- 42-angular/src/app/app.component.ts
+++ 42-angular/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {
   AngularFireDatabase,
@@ -11,30 +12,44 @@ import { interval, take } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public title = '42.1.png';
-  public imgURL: string = '42.1.png';
-  private imgURLList: string[] = [
-    '42.1.png',
-    '42.2.png',
-    '42.3.png',
-    '42.4.png',
-    '42.5.png',
-  ];
+  public title = '42app';
+  public step: number = 0;
+  public batchSFValue: string = '';
+  public val2: any = null;
+  public val3: any = null;
 
-  constructor(private db: AngularFireDatabase) {}
+  constructor(private db: AngularFireDatabase, private http: HttpClient) {}
 
   public startTrain(): void {
     interval(2000)
       .pipe(take(5))
       .subscribe((step) => {
-        console.log(step);
-        this.imgURL = this.imgURLList[step];
-      });
+        this.step = step;
 
-    console.log('Train started');
-    const batchSF: AngularFireObject<any> = this.db.object('Hackathon');
-    batchSF.valueChanges().subscribe((x) => {
-      console.log(x);
-    });
+        if (step === 0) {
+        }
+
+        if (step === 1) {
+          this.http.get('http://127.0.0.1:7101/42/two').subscribe((val) => {
+            this.val2 = val;
+          });
+        }
+
+        if (step === 2) {
+          this.http.get('http://127.0.0.1:7101/42/three').subscribe((val) => {
+            this.val3 = val;
+          });
+        }
+
+        if (step === 3) {
+        }
+
+        if (step === 4) {
+          const batchSF: AngularFireObject<any> = this.db.object('Hackathon');
+          batchSF.valueChanges().subscribe((x) => {
+            this.batchSFValue = x;
+          });
+        }
+      });
   }
 }
--- 42-angular/src/app/app.module.ts
+++ 42-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponentComponent } from './main-component/main-component.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, MainComponentComponent],
   imports: [
     AngularFireModule.initializeApp(environment.firebaseConfig),
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
